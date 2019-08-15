b655e37a4c9ae854dafe806fe5216d37a8bb9ddf
AlexCasanova9 <100346128@alumnos.uc3m.es>
2019-08-15 16:42:47 +0200

    Refactor GUI

--- app/src/main/java/com/example/foodanalyzerbot/presentation/home/fragments/ChatBotFragment.kt
+++ app/src/main/java/com/example/foodanalyzerbot/presentation/home/fragments/ChatBotFragment.kt
@@ -33,13 +33,31 @@ class ChatBotFragment : BaseFragment(), ChatBotView {
         }
     }
 
+
     override fun setBackgroundImage(resource: Int) {
         when (resource) {
             1 -> containerLayout.setBackgroundResource(R.drawable.chuche)
-            2 -> containerLayout.setBackgroundResource(R.drawable.fruta)
+            2 -> containerLayout.setBackgroundResource(R.drawable.galleta)
             3 -> containerLayout.setBackgroundResource(R.drawable.manzana)
             4 -> containerLayout.setBackgroundResource(R.drawable.sandia)
             5 -> containerLayout.setBackgroundResource(R.drawable.uva)
         }
+
+        /*
+        private String[] urls = {
+	        "https://drive.google.com/file/d/1bDWCxTRbTizJjGQz-bLZeMv1ZAywHexu/view"
+	        "https://drive.google.com/file/d/1bvxa-OiF7m2fBAEOvHUjItxY5SgpPIZn/view"
+	        "https://drive.google.com/file/d/1lhtd9k89jsYSuqK1KaJg6VcrLy8JvkCQ/view"
+	        "https://drive.google.com/file/d/1HscToRyUHTQBQ_97H_WtgsDam6ZkLly2/view"
+	        "https://drive.google.com/file/d/1puNxffyxmuCUniWpfsY-MS0wWRADV1zg/view"
+        };
+
+        private ImageView wallpaper;
+
+        wallpaper = (ImageView) findViewById(R.id.wallpaper);
+	        Glide.with(getApplicationContext())
+		    .load(urls[resource])
+		    .into(wallpaper);
+        */
     }
 }
--- app/src/main/java/com/example/foodanalyzerbot/presentation/home/view/ChatBotView.kt
+++ app/src/main/java/com/example/foodanalyzerbot/presentation/home/view/ChatBotView.kt
@@ -3,5 +3,6 @@ package com.example.foodanalyzerbot.presentation.home.view
 import com.example.foodanalyzerbot.presentation.common.BaseView
 
 interface ChatBotView : BaseView {
+
     fun setBackgroundImage(resource: Int)
 }
