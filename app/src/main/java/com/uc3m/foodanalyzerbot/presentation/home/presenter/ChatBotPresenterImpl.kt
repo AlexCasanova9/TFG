b704f38dd30575c41f5555e988e5379092b21ce5
Mario Manzano Culebras <mario.manzano@uxsmobile.com>
2019-08-24 22:16:04 +0200

    [DELETE] Fake Integration for DialogFlowInteractor to check calls.

--- app/src/main/java/com/uc3m/foodanalyzerbot/presentation/home/presenter/ChatBotPresenterImpl.kt
+++ app/src/main/java/com/uc3m/foodanalyzerbot/presentation/home/presenter/ChatBotPresenterImpl.kt
@@ -1,11 +1,16 @@
 package com.uc3m.foodanalyzerbot.presentation.home.presenter
 
+import com.uc3m.foodanalyzerbot.domain.interactor.InteractorCallback
+import com.uc3m.foodanalyzerbot.domain.interactor.dialogFlow.DialogFlowAsyncInteractor
 import com.uc3m.foodanalyzerbot.infrastructure.App
 import com.uc3m.foodanalyzerbot.presentation.common.BasePresenter
+import com.uc3m.foodanalyzerbot.presentation.home.model.MessageDto
 import com.uc3m.foodanalyzerbot.presentation.home.view.ChatBotView
 import java.util.*
 
 class ChatBotPresenterImpl : ChatBotPresenter, BasePresenter<ChatBotView>() {
+
+    private val dialogFlowInteractor = DialogFlowAsyncInteractor()
 
     override fun onClickPButton() {
 
@@ -18,7 +23,19 @@ class ChatBotPresenterImpl : ChatBotPresenter, BasePresenter<ChatBotView>() {
     }
 
     override fun onClickRandomWallpaper() {
-        view?.setBackgroundImage(randomNumber())
+        dialogFlowInteractor.execute("Hola", object : InteractorCallback<MessageDto> {
+            override fun success(data: MessageDto?) {
+                if (data != null && data.message.isNotEmpty())
+                    view?.showMessage(data.message)
+                else
+                    view?.showMessage("Mensaje vacío")
+            }
+
+            override fun fail(t: Throwable?) {
+                //Fail: Do what ever
+            }
+        })
+        //view?.setBackgroundImage(randomNumber())
     }
 
     override fun loadRandomBackground() {
